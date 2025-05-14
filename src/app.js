ac29251ed30c03491f8716302cc9637a456ee3b3
NamanNagpal5155 <namannagpal84@gmail.com>
2025-05-15 01:39:10 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -1,17 +1,26 @@
 const express=require('express');
 const app=express();/// create a new appilication // instance of express js application
-app.use("/",(req,res)=>{
-    console.log("hello everyone i am active");
+// app.use("/",(req,res)=>{
+//     console.log("hello everyone i am active");
+    
+// });
+app.get("/user",(req,res)=>{
+    console.log(req.query);
+    
+    res.send({
+        firstname:"naman",
+        lastname:"nagpal"
+    });
     
 });
-app.use("/hello",(req,res)=>{
-    console.log("hello everyone");
+app.post("/user",(req,res)=>{
+    res.send("hello everyone 123");
     
 });
-app.use((req,res)=>{
-    res.send("hello from the server.");
+app.delete("/user",(req,res)=>{
+    res.send("delete from the server.");
 });
 app.listen(3000, ()=>{
-    console.log("server is listening the port number=3000"); 
+    console.log("server is listening the port number 3000"); 
     
 });
